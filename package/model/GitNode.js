f46ccdbd22edf0b6c78313ed5994fc32443851cc
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-11 01:07:03 +0800

    ss

--- package/model/GitNode.js
+++ package/model/GitNode.js
@@ -1,22 +1,48 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 
-function runCommand(command){
-    exec(command,(err,res)=>{
-        if(err){throw new Error("Error Running Command")}
-        console.log("Command Run Sucessfully")
-    });
+function runCommand(command) {
+  exec(command, (err, res) => {
+    if (err) {
+      throw new Error("Error Running Command");
+    }
+    console.log(res)
+  });
 }
 
-function checkGit() {
-    const files = fs.readdirSync('./');
-    const isGitPresent = files.includes('.git');
-  
-    return isGitPresent;
+function gitnode() {
+  // Check if .git directory exists
+  const isGitPresent = fs.existsSync('.git');
+
+  // Check if Git is installed
+  runCommand('git --version');
+
+  if (!isGitPresent) {
+    console.error("Error Loading GitNode: Your repository doesn't have a .git folder.");
+    return;
   }
 
-class GitNode {
-    
+  return {
+    runGitCommand: function (command) {
+      runCommand(`git ${command}`);
+    },
+    autoCommit: function (message) {
+      const autoCommitScript = `
+        git status
+        git add .
+        git status
+        git commit -m "${message}"
+        git checkout main
+        git pull origin main
+        git merge your-feature-branch
+        git commit -m "Merge branch 'your-feature-branch' into main"
+        git push origin main
+      `;
+
+      runCommand(autoCommitScript);
+    }
+  };
 }
 
-module.exports = GitNode;
+
+module.exports = gitnode;
