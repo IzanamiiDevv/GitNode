77d9b216211d0f27ee3e18ca6985f5a3681b627b
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-10 16:33:35 +0800

    ss

--- package/index.js
+++ package/index.js
@@ -1,27 +1,13 @@
-const { exec } = require('child_process')
+const { exec } = require('child_process');
 
-function runNpmInstall() {
-  const command = exec('echo Hi');
-
-  command.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
-
-  command.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-
-  command.on('close', (code) => {
-    if (code === 0) {
-      console.log('npm install completed successfully.');
-    } else {
-      console.error(`npm install process exited with code ${code}.`);
+function TerminalCommand(command) {
+  exec(command,(err,stdout)=>{
+    if(err){
+        console.err("Error Running The", command)
     }
   });
 }
 
-function GitNode(){
-    console.log("GitNode Linked");
-}
 
-module.exports = runNpmInstall;
+
+module.exports = TerminalCommand;
